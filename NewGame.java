778cc0d911b209252f35af259499c92861bded60
ClaytonAllen <clayton9721@gmail.com>
2017-06-03 00:09:00 -0700

    Add files via upload

---
+++ NewGame.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.*;
+
+public class NewGame {
+
+	public NewGame() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("bengis");
+		Floor bengis = new Floor(175,60,10,2);
+		bengis.ask();
+	}
+}
